258be6851a9da85d75e1c70fbc1727b87008c605
Matt Speakman <m.speakman89@googlemail.com>
2014-11-27 12:30:02 +0000

    Create Credentials.cs

---
+++ Credentials.cs
@@ -0,0 +1,14 @@
+# The Credentials Model Class
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PartyInvites.Models
+{
+    public class Credentials
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
